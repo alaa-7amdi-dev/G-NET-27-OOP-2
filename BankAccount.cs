6ab60d3dc629d36b2578cd8a0e37dffa04d67519
alaa-7amdi-dev <hamdihafezalaa@gmail.com>
2026-02-21 15:01:28 +0200

    Q1

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_27_OOP_2
+{
+    internal class BankAccount
+    {
+        private string _owner=default!;
+        private double _balance;
+
+        public void Withdrow(double balane)
+        {
+            if (balane < 0) 
+            
+               throw new ArgumentOutOfRangeException(nameof(balane));
+              
+            
+            _balance = balane;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            //A:
+            //1=> Field must be private.
+            //2=>There needs to be a validation before modification.
+            //C:
+            // When you expose a field as public=> anyone can access the field (read && write) no validation 
+            //that conflicts with encapsulation principle.
+            #endregion
+
         }
     }
 }
